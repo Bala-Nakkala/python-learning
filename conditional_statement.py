297cfdff718ac1e33ddeeef242acf03dd3d6497c
Balu@N <balakrishnanakkala4567@gmail.com>
2026-05-06 05:54:01 +0000

    conditional_statement

---
+++ conditional_statement.py
@@ -0,0 +1,13 @@
+# Conditional Statement in Python
+# if statement
+age = 18
+if age > 15:
+    print("you are not eligible for voting")
+
+# if-else statement
+age = 18
+if age == 18:
+    print("you are eligible for voting")
+
+else:
+    print("you are not eligible for voting")
